046c633714b318b4a1284d2f7e102bb43545fd01
Ankit-015 <ankitlogin15@gmail.com>
2024-08-09 11:01:20 +0530

    added changes in steps

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
 const message = [
-  "Learn React",
-  "Apply for jobs",
-  "Invest your new income"
+  "Learn React 🧑‍🎓",
+  "Apply for jobs 🕹️",
+  "Invest your new income 🫡"
 ]
 
 function App() {
   return (
     <div>
       <Steps />
-      <Steps />
+      {/* <Steps /> */}
     </div>
   )
 }
@@ -39,14 +39,26 @@ function Steps() {
           <div className={`${step >= 2 ? 'active' : ""}`}>2</div>
           <div className={`${step >= 3 ? 'active' : ""}`}>3</div>
         </div>
-        <p className="message"> Step {step} : {message[step - 1]}</p>
+        <StepMessage step={step}> {message[step - 1]}</StepMessage>
         <div className="buttons">
-          <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handlePrevious} >Previous</button>
-          <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handleNext} >Next</button>
+          <Button textColor={'#fff'} bgColor={'#7950f2'} onClick={handlePrevious}><span>👈</span>Previous</Button>
+          <Button textColor={'#fff'} bgColor={'#7950f2'} onClick={handleNext}><span>👉</span>Next</Button>
+
         </div >
       </div>}
     </>
   )
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return <button style={{ backgroundColor: bgColor, color: textColor }} onClick={onClick} >{children}</button>
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children} </div>)
 }
 
 export default App;
